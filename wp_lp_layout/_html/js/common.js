55a8b4a52c19aca10dbf2276246c08638190e399
Maxim <chemaxa@gmail.com>
2015-03-12 17:00:01 +0300

    Make Header

--- wp_lp_layout/_html/js/common.js
+++ wp_lp_layout/_html/js/common.js
@@ -1,5 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	
-	
+	//Resize Bg
+	function wResize() {
+		$(".main_head").css("height", $(window).height())
+	};
+	wResize();
+	$(window).resize(function () {
+		wResize();
+	})
+
+	// Sandwich Menu
+	$(".sandwich, .menu_item").click(function () {
+		$(".sandwich").toggleClass("active");
+	});
+
+	//data-parallax="scroll" data-image-src="img/bg.jpg" data-z-index="1"
+
+	//Preloader
+	$(window).load(function () {
+		$(".loader_inner").fadeOut();
+		$(".loader").delay(400).fadeOut("slow");
+	});
 });
