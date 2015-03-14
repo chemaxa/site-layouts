72eb7e09c75d0a9cca1ae1aedebebe773ebcaffb
Maxim <chemaxa@gmail.com>
2015-03-14 13:57:04 +0300

    section about
    Add section about style & content

--- wp_lp_layout/_html/js/common.js
+++ wp_lp_layout/_html/js/common.js
@@ -16,9 +16,26 @@ $(document).ready(function () {
 	});
 
 
-	//Preloader
-	$(window).load(function () {
-		$(".loader_inner").fadeOut();
-		$(".loader").delay(400).fadeOut("slow");
+	// Magnific POPUP
+	$('.popup').magnificPopup({
+		type: 'image'
 	});
+
+});
+
+//Preloader
+$(window).load(function () {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+	// ANIMATION Header
+	$(".top_text h1").animated("fadeInDown", "fadeInUp");
+	$(".top_text p, .section_header, .top_menu ul").animated("fadeInUp", "fadeInDown");
+
+	// ANIMATION SEction About
+	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
+	$(".animation_1").animated("flipInY", "flipOutY");
+	$(".animation_3").animated("fadeInRight", "fadeOutRight");
+
+
+
 });
